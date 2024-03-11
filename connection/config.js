996dbd876945e5f677bbee0f273452b1169b5f5d
AjAy SHEOKAND <AjAySHEOKAND@AjAySHEOKAND.local>
2024-03-11 16:11:35 +0530

    Update URI

--- connection/config.js
+++ connection/config.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://localhost:27017/web-paint';
+// const MONGO_URI = 'mongodb://localhost:27017/web-paint';
+const MONGO_URI = 'mongodb+srv://ajaysheokand:nFXnOiBUuub4Igjf@cluster0.zzgh3iy.mongodb.net/web-paint';
+
+// mongodb+srv://ajaysheokand:Ajay12345@cluster0.yk7wj8a.mongodb.net/
 
 const dbConnection = () => {
     mongoose.connect(MONGO_URI, {
